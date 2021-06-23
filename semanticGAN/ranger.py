9d5e90f9824c3f1c2657ebf73bdadedec89549a4
Daiqing Li <daiqingl@nvidia.com>
2021-06-23 10:30:29 -0400

    update license

--- semanticGAN/ranger.py
+++ semanticGAN/ranger.py
@@ -23,28 +23,8 @@
 # changes 8/31/19 - fix references to *self*.N_sma_threshold;
 # changed eps to 1e-5 as better default than 1e-8.
 
-
-"""
-Copyright (C) 2021 NVIDIA Corporation.  All rights reserved.
-Licensed under The MIT License (MIT)
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-the Software, and to permit persons to whom the Software is furnished to do so,
-subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-"""
+# Apache License 2.0 LICENSE code copy from https://github.com/lessw2020/Ranger-Deep-Learning-Optimizer 
+# please refer to https://github.com/lessw2020/Ranger-Deep-Learning-Optimizer/blob/master/LICENSE
 
 
 import math
